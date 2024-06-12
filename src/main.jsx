6ebeca5c8f8ed4227d993314465640b6200f78bc
Shealyn Sugai <shealyn@student.dev-island.org>
2024-06-11 21:06:11 -1000

    cleaned up code

--- src/main.jsx
+++ src/main.jsx
@@ -8,10 +8,3 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <App />
   </React.StrictMode>
 );
-
-// import React from "react";
-// import ReactDOM from "react-dom";
-
-// import App from "./App.jsx";
-
-// ReactDOM.render(<App />, document.getElementById("app"));
